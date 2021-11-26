c5baec2190ae1d1b173d27c20613695cfe414b94
sakshisaxena24 <sakshi.saxena19@gmail.com>
2021-11-27 02:19:34 +0530

    Updating hackerrank 1,2

---
+++ src/main/java/com/java/Even_Odd_If_Else.java
@@ -0,0 +1,64 @@
+/* Java If-Else
+In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes.
+Task 
+Given an integer, n, perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+Input Format
+A single line containing a positive integer, n.
+Constraints
+1 <= n <= 100
+Output Format
+Print Weird if the number is weird; otherwise, print Not Weird. */
+//
+//If  is odd, print Weird
+//If  is even and in the inclusive range of 2 to 5, print Not Weird
+//If  is even and in the inclusive range of 6 to 20, print Weird
+//If  is even and greater than 20, print Not Weird
+//Complete the stub code provided in your editor to print whether or not  is weird.
+//
+//Input Format
+//
+//A single line containing a positive integer, .
+//
+//Constraints
+//
+//Output Format
+//
+//Print Weird if the number is weird; otherwise, print Not Weird.
+
+package com.java;
+
+import java.util.Scanner;
+
+public class Even_Odd_If_Else {
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int N = scanner.nextInt();
+
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		scanner.close();
+
+		if (N % 2 == 0) {
+			if (N >= 2 && N <= 5) {
+				System.out.println("Not Weird");
+			}
+			else if(N>=6 && N<=20) {
+				System.out.println("Weird");
+				
+			}
+			else {
+				System.out.println("Not Weird");
+			}
+
+		} else {
+			System.out.println("Weird");
+		}
+
+	}
+}
---
+++ src/main/java/com/java/RangeSize.java
@@ -0,0 +1,12 @@
+package com.java;
+import java.lang.*;
+public class RangeSize {
+	
+	public static void main(String args[]) {
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.BYTES);
+		
+	}
+
+}
